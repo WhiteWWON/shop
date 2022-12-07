5d72975fa44edf305a534ca61b798eb03e5f329b
WhiteWWON <wonjae6707@gmail.com>
2022-12-07 22:32:03 +0900

    변경 2 - 회원 정보 수정

---
+++ src/main/java/com/shop/dto/MemberInfoCheckFormDto.java
@@ -0,0 +1,30 @@
+package com.shop.dto;
+
+import com.shop.entity.Member;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.modelmapper.ModelMapper;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+public class MemberCheckFormDto {
+
+    private Long id;
+
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
+    private String email;
+
+    @NotEmpty(message = "비밀번호 확인을 위해 입력해 주세요.")
+    @Length(min=8, max=20, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
+    private String password;
+
+    private static ModelMapper modelMapper = new ModelMapper();
+    public static MemberCheckFormDto of(Member member){
+        return modelMapper.map(member, MemberCheckFormDto.class);
+    }
+}
